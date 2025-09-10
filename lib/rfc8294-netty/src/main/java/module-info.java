7f4f6bd724ab35db0a5ca9ffe7916580e620f3c1
Robert Varga <robert.varga@pantheon.tech>
2025-09-10 14:23:30 +0200

    Add odl-ietf-rfc8294-netty
    We havfe a straggler artifact from MD-SAL migration, add it.
    
    Change-Id: I437eed6baf9eca1db966cbe98d9429e2f3381470
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ lib/rfc8294-netty/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.ietf.rfc8294.netty {
+    exports org.opendaylight.ietf.rfc8294.netty;
+
+    requires transitive io.netty.buffer;
+    requires transitive org.opendaylight.yangtools.yang.common;
+    requires transitive org.opendaylight.yang.gen.ietf.routing.types.rfc8294;
+
+    // Annotations
+    requires static org.eclipse.jdt.annotation;
+    requires static org.osgi.annotation.bundle;
+}
+
